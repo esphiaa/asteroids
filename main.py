35d5556e00006bc57879c3cfa2e00bbd56389d04
esphiaa <kyuwlol@gmail.com>
2025-05-22 14:24:09 +0100

    GUYS theres a player now

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import pygame
 from constants import *
+from player import *
+
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+clock = pygame.time.Clock()
+dt = 0 # delta time
 
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # Check if game is closed, end loop if so
+    player = Player(x, y)
+
+    # Game loop
     while True:
+        # Check if game is closed, end loop if so
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         # Set bg to black
         pygame.Surface.fill(screen, (0, 0, 0))  # Fill the screen with black
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 # Set the frame rate to 60 FPS (pause for 1/60 seconds)
 
 if __name__ == "__main__":
     main()
